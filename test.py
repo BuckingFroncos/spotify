447ee9733b549d8cffe7bd07985b52acaf6f701c
Bryan Trinh <bryantrinh@Bryans-MacBook-Pro.local>
2022-02-21 14:10:08 -0800

    added related artists & debug print statements

--- test.py
+++ test.py
@@ -15,11 +15,16 @@
 
 # Imports ##################################################
 ############################################################
+from email.mime import audio
 import os
 
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
+# DEBUG ####################################################
+############################################################
+DEBUG_print = True
+DEBUG_onlyartist = False
 
 # Functions ################################################
 ############################################################
@@ -46,48 +51,70 @@ def test_global5():
   for track in sp.playlist_tracks(playlist_URI)["items"]:
     # song list
     slist = []
+
+    # Main Artist
+    artist_uri = track["track"]["artists"][0]["uri"]
+    artist_info = sp.artist(artist_uri)
+    slist.append(artist_info["name"])
+    slist.append("<...>")
+    if not DEBUG_onlyartist:
+      slist.append("Artist uri: " + artist_uri)
     
     # URI
     track_uri = track["track"]["uri"]
     audio_features = sp.audio_features(track_uri)[0]
-    slist.append(track_uri)
-    slist.append(audio_features)
+    if not DEBUG_onlyartist:
+      slist.append("Track uri: " + track_uri)
+      slist.append("Audio features: ")
+      for x in audio_features:
+        slist.append("  " + x + ": " + str(audio_features[x]))
     
     # Track name
     track_name = track["track"]["name"]
-    slist.append(track_name)
-    
-    # Main Artist
-    artist_uri = track["track"]["artists"][0]["uri"]
-    artist_info = sp.artist(artist_uri)
-    slist.append(artist_uri)
-    slist.append(artist_info)
+    if not DEBUG_onlyartist:
+      slist.append("Track name: " + track_name)
     
     # Name, popularity, genre
     artist_name = track["track"]["artists"][0]["name"]
     artist_pop = artist_info["popularity"]
     artist_genres = artist_info["genres"]
-    slist.append(artist_name)
-    slist.append(artist_pop)
-    slist.append(artist_genres)
+    if not DEBUG_onlyartist:
+      slist.append("Artist name: " + artist_name)
+      slist.append("Artist pop: " + str(artist_pop))
+      slist.append("Artist genres: ")
+      for x in artist_genres:
+        slist.append("  " + x)
     
     # Album
     album = track["track"]["album"]["name"]
-    slist.append(album)
+    if not DEBUG_onlyartist:
+      slist.append("Album: " + album)
     
     # Popularity of the track
     track_pop = track["track"]["popularity"]
-    slist.append(track_pop)
+    if not DEBUG_onlyartist:
+      slist.append("Track pop: " + str(track_pop))
+
+    # Similar artists
+    sa_count = 0
+    similar_artist = sp.artist_related_artists(artist_uri)
+    slist.append("5 Related artists: ")
+    for x in similar_artist["artists"]:
+      slist.append("  " + x["name"])
+      sa_count += 1
+      if sa_count > 5:
+        break
 
     # Printing
-    print("Track #" + str(count))
-    count += 1
-    for x in slist:
-      print(x)
-    
-    print("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
-    if count > 5:
-     break
+    if DEBUG_print:
+      print("Track #" + str(count))
+      count += 1
+      for x in slist:
+        print(x)
+      
+      print("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
+      if count > 5:
+        break
 
 # Main #####################################################
 ############################################################
