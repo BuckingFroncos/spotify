fd6553dfc9fe202027dec9d4b62b51e3995355f0
Jesus Ruelas <91504943+Jesus711@users.noreply.github.com>
2022-02-22 20:26:45 -0800

    Created Basic Web Page Layout and Simple Flask Application

---
+++ app.py
@@ -0,0 +1,18 @@
+# Simple Flask Application for basic layout of our Project
+# Must have Flask Module installed, pip intall Flask
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/artists/')
+def test():
+    return render_template('artists.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True) # Debug=True, allows us to see the specific errors of the application
