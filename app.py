1887d7db399418ed58ac3856edf0eee569be5724
Jesus Ruelas <91504943+Jesus711@users.noreply.github.com>
2022-03-01 13:17:38 -0800

    Added new URL that uses the module Pillow

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from flask import Flask, render_template, url_for
 from test3 import *
 import requests
 import json
+import urllib # Used to get images from web using their url found in image html tags
+from PIL import Image, ImageFilter # Used to manipulate images extraction from webpage
 
 app = Flask(__name__)
 
@@ -42,6 +44,37 @@ def apiTest():
     req = requests.get("https://v2.jokeapi.dev/joke/Any?type=single&contains=programming")
     data = json.loads(req.content)
     return render_template("apiTest.html", data=data["joke"])
+
+
+@app.route('/Pillow/')
+def apiPillow():
+    """Function uses module Pillow. Pillow allows to manipulate images and save them to our computer.
+    This method will parse through html of allrecipes.com and save all images' url if it begins with 'https'
+    FOUR images will be filtered with Sharpen and downloaded and saved onto the users computer. The orginal images
+    will also be displayed on the url"""
+    page = requests.get("https://www.allrecipes.com")
+    soup = BeautifulSoup(page.content, "html.parser")
+    urls = soup.findAll('img')
+    images = [] # Holds the names of images that have been retrieved
+    image_urls = [] # Holds the urls of images 
+    img_num = 1 # Counter
+    for i in urls[30:]:
+        pic = i.attrs['src']
+        if pic[0:5] == "https":
+            extension = pic[-4:] if pic[-4:] == ".png" or pic[-4:] == '.jpg' else pic[-5:]
+            name = str(img_num) + extension
+            images.append(urllib.request.urlretrieve(pic,name)[0]) # Save image as counter + entension
+            image_urls.append(pic) # saves image url to display on the web page
+            img_num += 1
+        if img_num == 5:
+            break
+
+    for i in images: # Module Pillow, PIL USED: Adds a filter to increase sharpness for each image and saves the image
+        img = Image.open(i)
+        out = img.filter(ImageFilter.SHARPEN)
+        out.save(i)
+    
+    return render_template('extraction.html', images = image_urls) # Passes image urls to display each image on the webpage
 
 
 if __name__ == "__main__":
