b09ad5aeed66cb456bb28f7fcb5e5954b8a09dc9
Jesus Ruelas <91504943+Jesus711@users.noreply.github.com>
2022-03-10 12:02:31 -0800

    Added Unit Tests for Elements on /Search Page

--- auto-spot-app/src/App.test.js
+++ auto-spot-app/src/App.test.js
@@ -1,8 +1,24 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import Home from './pages/Home'
+import PlaylistCreate from './pages/MusicSearch'
 
 test('check to see for Heading in Home page', () => {
   render(<Home />);
   const linkElement = screen.getByText(/Welcome/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+
+test('Renders Search button on secondary page /Search', () => {
+  render(<PlaylistCreate/>);
+  const buttonElement = screen.getByText(/Search For Artist/i);
+  expect(buttonElement).toBeInTheDocument();
+});
+
+
+test('Renders Create Playlist button on secondary page /Search', () => {
+  render(<PlaylistCreate/>);
+  const buttonElement = screen.getByText(/Create a New Playlist/i);
+  expect(buttonElement).toBeInTheDocument();
+});
+
