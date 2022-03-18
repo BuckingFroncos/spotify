d96abec861eff9e848ebaa77c30d2dc0cb0b24f0
Jesus Ruelas <91504943+Jesus711@users.noreply.github.com>
2022-03-17 18:38:58 -0700

    Modified Dataset to be able to Display Artist's Name and Image if available from User's Input

--- auto-spot-app/src/pages/ArtistsList.js
+++ auto-spot-app/src/pages/ArtistsList.js
@@ -1,22 +1,26 @@
-import { makeStyles } from '@material-ui/core';
-const useStyles = makeStyles({
-    artistList: {
-        width: "250px"
-    }
-})
-
+import '../index.css'
 
 const ArtistsList = ({data}) => {
-    const classes = useStyles()
-    const artists = []
-    for(var artist in data) {
-        artists.push([artist, artist[artist]])
+    const dataset = {};
+    let arrLength = 0;
+    for(var name in data){
+        console.log(name)
+        console.log(data[name][1][0])
+        arrLength = data[name][1].length;
+        if (arrLength > 0){
+            console.log(data[name][1][0]['url'])
+            dataset[name] = data[name][1][0]['url']
+        }
+        else{
+            dataset[name] = "No Image Available"
+        }
     }
 
     return (
-        <div className={classes.artistList}>
-            {artists.map((name) => (
-                <div key={name}>
+        <div className="artistList">
+            {Object.entries(dataset).map(([name, url]) => (
+                <div className="artist" key={name,url}>
+                    <img className="artist-pic" src={url} alt={url}/>
                     <li>{name}</li>
                 </div>
             ))}
