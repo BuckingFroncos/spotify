bb6e55be9ff8a772bfa5bc3bdd4c3ad3cc1fec9b
nhi-nguyen-csis <nhi.nguyen.csis@gmail.com>
2022-05-06 19:22:53 -0700

    updated app icon

--- Spotify/Spotify/Managers/APICaller.swift
+++ Spotify/Spotify/Managers/APICaller.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+final class APICaller {
+    static let shared = APICaller()
+    private init(){}
+    
+//    public func getCurrentUserAlbums(completion: @escaping (Result<UserProfile, Error>) -> Void) {
+//        createRequest(
+//            with: URL(string: Constants.baseAPIURL + "/me/albums"),
+//            type: .GET
+//        ) { request in
+//            let task = URLSession.shared.dataTask(with: request) { data, _, error in
+//                guard let data = data, error == nil else {
+//                    completion(.failure(APIError.faileedToGetData))
+//                    return
+//                }
+//
+//                do {
+//                    let result = try JSONDecoder().decode(LibraryAlbumsResponse.self, from: data)
+//                    completion(.success(result.items.compactMap({ $0.album })))
+//                }
+//                catch {
+//                    completion(.failure(error))
+//                }
+//            }
+//            task.resume()
+//        }
+//    }
+}
--- Spotify/Spotify/Managers/AuthManager.swift
+++ Spotify/Spotify/Managers/AuthManager.swift
@@ -14,6 +14,8 @@ final class AuthManager {
         static let clientID = "99a370ff3ffe4ac7a516cf3bbe8982e7"
         static let clientSecret = "79a68c935a574698b3f43f7e0aae8907"
         static let tokenAPIURL = "https://accounts.spotify.com/api/token"
+        static let redirectURI = "https://www.codepath.org"
+        static let scopes = "user-read-private%20playlist-modify-public%20playlist-read-private%20playlist-modify-private%20user-follow-read%20user-library-modify%20user-library-read%20user-read-email"
     }
     
     private init(){
@@ -22,9 +24,8 @@ final class AuthManager {
     
     public var signInURL: URL?{
         let base = "https://accounts.spotify.com/authorize"
-        let scopes = "user-read-private"
-        let redirectURI = "https://www.codepath.org"
-        let string = "\(base)?response_type=code&client_id=\(Constants.clientID)&scope=\(scopes)&redirect_uri=\(redirectURI)&show_dialog=true"
+
+        let string = "\(base)?response_type=code&client_id=\(Constants.clientID)&scope=\(Constants.scopes)&redirect_uri=\(Constants.redirectURI)&show_dialog=true"
         return URL(string: string)
     }
     
@@ -64,7 +65,8 @@ final class AuthManager {
         components.queryItems = [
             URLQueryItem(name: "grant_type", value: "authorization_code"),
             URLQueryItem(name: "code", value: code),
-            URLQueryItem(name: "redirect_uri", value: "https://www.codepath.org")
+            URLQueryItem(name: "redirect_uri", value: Constants.redirectURI)
+            
         ]
         
         var request = URLRequest(url: url)
@@ -105,13 +107,86 @@ final class AuthManager {
         
     }
     
-    public func refreshAccessToken(){
+//    public func withValidToken(completion: @escaping (String) -> Void){
+//        if shouldRefreshToken {
+//            // refresh
+//            refreshIfNeeded { success in
+//                if let token = self?.accessToken, success {
+//                    completion(token)
+//                }
+//
+//            }
+//        } else if let token = accessToken {
+//            completion(token)
+//        }
+//    }
+    
+    public func refreshIfNeeded(completion: @escaping (Bool) -> Void){
+        guard shouldRefreshToken else {
+            completion(true)
+            return
+        }
+        guard let refreshToken = refreshToken else {
+            return
+        }
+        
+        // refresh the token
+        guard let url = URL(string: Constants.tokenAPIURL) else {
+            return
+        }
+        
+        var components = URLComponents()
+        components.queryItems = [
+            URLQueryItem(name: "grant_type", value: "refresh_token"),
+            URLQueryItem(name: "refresh_token", value: refreshToken)
+        ]
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/x-www-form-urlencoded ",
+                         forHTTPHeaderField: "Content-Type")
+        request.httpBody = components.query?.data(using: .utf8)
+
+        let basicToken = Constants.clientID+":"+Constants.clientSecret
+        let data = basicToken.data(using: .utf8)
+        guard let base64String = data?.base64EncodedString() else {
+                    print("Failure to get base64")
+                    completion(false)
+                    return
+        }
+        
+        request.setValue("Basic \(base64String)",
+                                 forHTTPHeaderField: "Authorization")
+        
+        
+        let task = URLSession.shared.dataTask(with: request) { [weak self] data, _, error in
+            guard let data = data,
+                    error == nil else {
+                completion(false)
+                return
+            }
+            
+            do {
+                let result = try JSONDecoder().decode(AuthResponse.self, from: data)
+                print("successfully refreshed")
+                self?.cacheToken(result: result)
+                completion(true)
+            } catch {
+                print(error.localizedDescription)
+                completion(false)
+            }
+        }
+        task.resume()
         
     }
     
     private func cacheToken(result: AuthResponse){
-        UserDefaults.standard.setValue(result.access_token, forKey: "access_token")
-        UserDefaults.standard.setValue(result.refresh_token, forKey: "refresh_token")
+        UserDefaults.standard.setValue(result.access_token,
+                                       forKey: "access_token")
+        if let refresh_token = result.refresh_token {
+            UserDefaults.standard.setValue(refresh_token,
+                                           forKey: "refresh_token")
+        }
         UserDefaults.standard.setValue(Date().addingTimeInterval(TimeInterval(result.expires_in)), forKey: "expirationDate")
     }
     
